2c5ee356f4bc3c2e3861b10468bcd8a1dcc112e0
ZIZizou <atharva.naik@ucalgary.ca>
2021-03-28 00:00:12 +0400

    removed unecessary files

--- djangonautic/djangonautic/views.py
+++ djangonautic/djangonautic/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def homepage(request):
 	# return HttpResponse('homepage')
-	return render(request,'test3.html')
+	return render(request,'test.html')
 def about(request):
 	# return HttpResponse('about')
 	return render(request,'about.html')
