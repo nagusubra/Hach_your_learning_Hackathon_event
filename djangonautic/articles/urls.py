462c9b35daf22ae2a8d9c7bcff0d3a404e7d837a
ZIZizou <atharva.naik@ucalgary.ca>
2021-03-27 23:43:55 +0400

    changes for testing

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='articles'
 urlpatterns = [
     path('',views.article_list,name="list"),
-    path('<slug:slug>', views.article_detail,name="detail"),
+    #path('<slug:slug>', views.article_detail,name="detail"),
+    path('test3/', views.test3, name = 'test3')
 ]
