2cb2127c33350dc36e1ccf3a8b39ef6f60ed99ee
rishabh9898 <rishabh.ruhela@ucalgary.ca>
2021-03-28 00:56:47 +0400

    ---newfold--

---
+++ DjangoRish/djangonautic/articles/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+app_name='articles'
+urlpatterns = [
+    path('',views.article_list,name="list"),
+    path('<slug:slug>', views.article_detail,name="detail"),
+    # path('test3/', views.test3, name = 'test3')
+]
+ 
