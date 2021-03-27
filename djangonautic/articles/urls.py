b57f637af41b583c822ee118f12e0829123dad12
ZIZizou <atharva.naik@ucalgary.ca>
2021-03-28 00:11:40 +0400

    made changes to the app to accept home.html and css

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 app_name='articles'
 urlpatterns = [
-    path('',views.landing,name="list"),
+    path('',views.landing,name="landing"),
+    path('/',views.landing,name="landing"),
 ]
