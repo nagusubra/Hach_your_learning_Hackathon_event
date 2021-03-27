47493cef740186a52f24f393fb9b1e304ff22048
ZIZizou <atharva.naik@ucalgary.ca>
2021-03-27 22:57:22 +0400

    minor changes

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 app_name='articles'
 urlpatterns = [
-    path("",views.article_list,name="list"),
+    path('',views.article_list,name="list"),
     path('<slug:slug>', views.article_detail,name="detail"),
 ]
--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -10,3 +10,4 @@ def article_detail(request,slug):
 	# return HttpResponse(slug) 
 	article = Article.objects.get(slug=slug)
 	return render(request,'articles/article_detail.html',{'article':article})
+
