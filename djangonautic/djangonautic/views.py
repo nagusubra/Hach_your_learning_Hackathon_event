e28f82e912b61abe5c986d05ccb1623ae39aefad
rishabh9898 <rishabh.ruhela@ucalgary.ca>
2021-03-27 23:06:26 +0400

    changes

--- djangonautic/djangonautic/views.py
+++ djangonautic/djangonautic/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def homepage(request):
 	# return HttpResponse('homepage')
-	return render(request,'homepage.html')
+	return render(request,'test3.html')
 def about(request):
 	# return HttpResponse('about')
 	return render(request,'about.html')
