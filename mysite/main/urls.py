53b24c8bb7c392bd8e0435ad4b5d00eb40535ef2
ZIZizou <atharva.naik@ucalgary.ca>
2021-03-28 14:04:57 +0400

    changed files to import new templates

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,5 +4,4 @@ from . import views
 app_name='main'
 urlpatterns = [
     path('',views.landing,name="landing"),
-    path('chat', views.chat, name = "chat"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,12 +6,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 def landing(request):
-	return render(request,'main/landing.html')
+	return render(request,'main/home.html')
 
-def chat(request):
-	return render(request,'main/chat.html')
 
-def signup_view(request):
-	form= UserCreationForm()
-	return render(request,'main/signup.html',{'form':form})
+
 
