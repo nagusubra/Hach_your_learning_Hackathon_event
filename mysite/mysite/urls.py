d58c55d83d646b57e87fc2319d2e55b32dc9c535
ZIZizou <atharva.naik@ucalgary.ca>
2021-03-28 18:28:24 +0400

    changes to make log in a bit more navigable and interactive

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
-    path('wmove/', include('main.urls'))
+    path('wmove/', include('main.urls')),
+    path('personal/', include('accounts.urls'))
 ]
